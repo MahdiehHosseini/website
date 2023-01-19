4fbbc7a5f8a2dc854d3e063c7fe061b248b49cdf
Mahdieh Hosseini <mahdieh1384.sh@gmail.com>
2023-01-19 12:50:27 +0330

    publish last update

--- src/App.jsx
+++ src/App.jsx
@@ -27,14 +27,14 @@ function App() {
           <Routes>
             <Route path='/' element={<HomePage />} />
             <Route path='/aboutme' element={<AboutMe />} />
-            <Route path='/skills' element={<Skills />} />
-            <Route path='/portfolio' element={<Portfolio />} />
+            <Route path='/skills' element={<Skills data={[{Title: '1', Percent: 96},{Title: '2', Percent: 50},{Title: '3', Percent: 96},{Title: '4', Percent: 50},{Title: '5', Percent: 96},{Title: '6', Percent: 50}]} />} />
+            <Route path='/portfolio' element={<Portfolio data={[{Title: 'home', ImageName: '/src/assets/portfolio-images/image.jpg', Address: '#'},{Title: 'home', ImageName: '/src/assets/portfolio-images/image.jpg', Address: '#'},{Title: 'home', ImageName: '/src/assets/portfolio-images/image.jpg', Address: '#'}]}/>} />
             <Route path='/contact' element={<Contact />} />
           </Routes>
         </BrowserRouter>
       </Suspense>
       {/*classes*/}
-      <span className='hidden font-convergence font-vazir bg-black text-neutral-400 text-neutral-600 bg-white bg-light-black bg-light-black/70 bg-white/70 text-white text-black border-black border-neutral-400 placeholder:text-neutral-400 border-neutral-600 placeholder:text-neutral-600 bg-neutral-800 bg-neutral-200 border-white'></span>
+      <span className='hidden font-convergence font-vazir bg-black text-neutral-400 text-neutral-600 bg-white bg-light-black bg-light-black/70 bg-white/70 text-white text-black border-black border-neutral-400 placeholder:text-neutral-400 border-neutral-600 placeholder:text-neutral-600 bg-neutral-800 bg-neutral-200 border-white w-[50%]'></span>
     </div>
   )
 }
--- src/components/pages/Portfolio.jsx
+++ src/components/pages/Portfolio.jsx
@@ -1,36 +1,33 @@
 //import images
-import image from './../../assets/portfolio-images/image.jpg'
 import { useSelector } from 'react-redux'
 
-function Portfolio(){
+function Portfolio({data}){
     const them = useSelector((state) => state.toggle.them)
     const lang = useSelector((state) => state.toggle.lang)
+    const content = []
+    for(let i = 0 ; Math.ceil(data.length / 2) > i ; i++){
+        content.push(<div className='flex flex-col items-center w-full lg:flex-row lg:justify-around'>
+                        {<div className={`${data[(2*i)+1] ? 'animate__fadeInLeft' : 'lg:self-center'} my-14 relative md:flex md:self-start animate__animated animate__slow ${ !(data[(2*i)+1]) && (window.innerWidth > 1024) ? 'animate__fadeInUp' : 'animate__fadeInLeft'}`}>
+                            <img className="h-48 rounded-2xl" src={data[2*i].ImageName}></img>
+                            <span className='flex justify-center'>
+                                <p className={`text-xl md:text-2xl bg-${them.mainBgColor} py-1 px-3 md:left-32 absolute -bottom-4 rounded-lg`}>{lang === 'EN' ? 'Home' : 'خانه'}</p>
+                            </span>
+                        </div>}
+                        {data[(2*i)+1] && <div className="relative md:self-end md:flex lg:self-center animate__animated animate__fadeInRight animate__slow">
+                            <img className=" h-48 rounded-2xl" src={data[(2*i)+1].ImageName}></img>
+                            <span className='flex justify-center'>
+                                <p className={`text-xl md:text-2xl bg-${them.mainBgColor} py-1 px-3 md:left-32  absolute -bottom-4 rounded-lg`}>{lang === 'EN' ? 'Home' : 'خانه'}</p>
+                            </span>
+                        </div>}
+                    </div>)
+    }
     return(
         <div className='h-full md:mr-36 lg:mr-48 overflow-x-hidden'>
             <span className="flex justify-center mt-10">
                 <h3 className={`text-4xl border-b animate__animated animate__fadeInDown animate__slow border-${them.mainTxtColor} p-3`}>{lang === 'EN' ? 'Portfolio' : 'نمونه کارها'}</h3>
             </span>
             <div className="mx-16 lg:mx-10 flex flex-col items-center">
-                <div className='flex flex-col items-center w-full lg:flex-row lg:justify-around'>
-                    <div className="my-14 relative md:flex md:self-start animate__animated animate__fadeInLeft animate__slow">
-                        <img className=" h-48 rounded-2xl" src={image}></img>
-                        <span className='flex justify-center'>
-                            <p className={`text-xl md:text-2xl bg-${them.mainBgColor} py-1 px-3 md:left-32 absolute -bottom-4 rounded-lg`}>{lang === 'EN' ? 'Home' : 'خانه'}</p>
-                        </span>
-                    </div>
-                    <div className="relative md:self-end md:flex lg:self-center animate__animated animate__fadeInRight animate__slow">
-                        <img className=" h-48 rounded-2xl" src={image}></img>
-                        <span className='flex justify-center'>
-                            <p className={`text-xl md:text-2xl bg-${them.mainBgColor} py-1 px-3 md:left-32  absolute -bottom-4 rounded-lg`}>{lang === 'EN' ? 'Home' : 'خانه'}</p>
-                        </span>
-                    </div>
-                </div>
-                <div className={`my-14 relative md:self-start md:flex lg:self-center animate__animated animate__slow ${ window.innerWidth > 1024 ? 'animate__fadeInUp' : 'animate__fadeInLeft'}`}>
-                    <img className=" h-48 rounded-2xl" src={image}></img>
-                    <span className='flex justify-center'>
-                        <p className={`text-xl md:text-2xl bg-${them.mainBgColor} py-1 px-3 absolute md:left-32  -bottom-4 rounded-lg`}>{lang === 'EN' ? 'Home' : 'خانه'}</p>
-                    </span>
-                </div>
+                {content}
             </div>
         </div>
     )
